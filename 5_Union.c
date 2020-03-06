753071b91b9d7b7af725375a55253f202b498e18
blossom31s <44717935+blossom31s@users.noreply.github.com>
2020-03-06 22:13:03 +0900

    Update 5_Union.c

--- 5_Union.c
+++ 5_Union.c
@@ -8,7 +8,7 @@ struct Test1 {
     char a3;
 };
 
-union Test2 { // 공용체는 하나의 기억장소를 공유함(a1, a2, a3가 같은 기억장소를 공유함)
+union Test2 { // 공용체는 하나의 기억장소를 공유함
     int a1;
     short a2;
     char a3;
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
     printf("t1.a3 : %d\n", t1.a3);
 
     union Test2 t2;
-    t2.a1 = 65535;
+    t2.a1 = 65535; // a1, a2, a3가 같은 기억장소를 공유함
     printf("t2.a1 : %d\n", t2.a1);
     printf("t2.a2 : %d\n", t2.a2);
     printf("t2.a3 : %d\n", t2.a3);
